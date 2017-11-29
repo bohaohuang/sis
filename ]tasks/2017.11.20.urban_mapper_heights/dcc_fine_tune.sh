d4f6c8e0c0b96da60d257a8704d9ec4d4e3e61c2
bohaohuang <bohao.huang@duke.edu>
2017-11-29 08:37:29 -0500

    grid exp 1

--- ]tasks/2017.11.20.urban_mapper_heights/dcc_fine_tune.sh
+++ ]tasks/2017.11.20.urban_mapper_heights/dcc_fine_tune.sh
@@ -2,7 +2,8 @@
 #SBATCH -e slurm.err
 #SBATCH --mem=20G
 #SBATCH -c 6
-#SBATCH -p collinslab --gres=gpu:1
+#SBATCH --exclude=dcc-gpu-32
+#SBATCH -p gpu-common --gres=gpu:1
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/opt/apps/rhel7/cudnn/lib64:$LD_LIBRARY_PATH
 export PYTHONPATH=$PYTHONPATH:/dscrhome/bh163/code/sis
