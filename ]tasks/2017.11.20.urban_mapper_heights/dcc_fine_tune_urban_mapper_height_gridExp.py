5d51d8abede458650b1a5e2f12770c2558a2e7ae
bohaohuang <bohao.huang@duke.edu>
2017-11-29 14:31:05 -0500

    grid exp 5

--- ]tasks/2017.11.20.urban_mapper_heights/dcc_fine_tune_urban_mapper_height_gridExp.py
+++ ]tasks/2017.11.20.urban_mapper_heights/dcc_fine_tune_urban_mapper_height_gridExp.py
@@ -204,7 +204,7 @@ if __name__ == '__main__':
     for ly2kp in range(7, 10):
         layers_to_keep_num = [i for i in range(1, ly2kp+1)]
         #for lr in [0.5, 0.25, 0.1, 0.075, 0.05, 0.025, 0.01]:
-        for lr in [0.025]:
+        for lr in [0.01]:
             learning_rate = lr * lr_base
 
             model_name = '{}_rescaled_EP-{}_DS-{}_DR-{}_LY-{}_LR-{}-{:1.1e}'.format(flags.pre_trained_model.split('/')[-1],
