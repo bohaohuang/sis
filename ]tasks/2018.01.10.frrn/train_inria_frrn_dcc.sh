3beeb913d495c4ca984419240946d790d37ce1dc
bohaohuang <bohao.huang@duke.edu>
2018-01-11 11:00:28 -0500

    shell script

---
+++ ]tasks/2018.01.10.frrn/train_inria_frrn_dcc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -e slurm.err
+#SBATCH --mem=20G
+#SBATCH -c 6
+#SBATCH --exclude=dcc-gpu-[31-32]
+#SBATCH --exclude=dcc-collinslab-gpu-[02,04]
+#SBATCH -p collinslab --gres=gpu:1
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/opt/apps/rhel7/cudnn/lib64:$LD_LIBRARY_PATH
+export PYTHONPATH=$PYTHONPATH:/dscrhome/bh163/code/uab
+python train_inria_frrn_cluster.py
