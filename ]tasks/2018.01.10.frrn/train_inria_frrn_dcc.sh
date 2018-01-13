7d2ceee94908222eb5a5664d24ef47883ad057c7
bohaohuang <bohao.huang@duke.edu>
2018-01-13 13:39:47 -0500

    scripts for dcc grid jobs

--- ]tasks/2018.01.10.frrn/train_inria_frrn_dcc.sh
+++ ]tasks/2018.01.10.frrn/train_inria_frrn_dcc.sh
@@ -8,4 +8,4 @@
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/opt/apps/rhel7/cudnn/lib64:$LD_LIBRARY_PATH
 export PYTHONPATH=$PYTHONPATH:/dscrhome/bh163/code/uab
-python train_inria_frrn_cluster.py
+python train_inria_frrn_dcc.py --run-id=1 --GPU=0 --learning-rate=1e-3
