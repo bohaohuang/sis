25aee43566c7df2de56c39a62c01617cb503501d
bohaohuang <bohao.huang@duke.edu>
2018-02-10 10:33:13 -0500

    deeplab psbs module load

--- ]tasks/2018.01.10.frrn/train_inria_deeplab_psbs.sh
+++ ]tasks/2018.01.10.frrn/train_inria_deeplab_psbs.sh
@@ -5,6 +5,7 @@
 #SBATCH --exclude=dcc-gpu-[31-32]
 #SBATCH --exclude=dcc-collinslab-gpu-[02,04]
 #SBATCH -p gpu-common --gres=gpu:1
+module load Anaconda3/3.5.2 
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/opt/apps/rhel7/cudnn/lib64:$LD_LIBRARY_PATH
 export PYTHONPATH=$PYTHONPATH:/dscrhome/bh163/code/uab
