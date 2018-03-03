e2e5bc53e43b033a05979caac7fe4ab3df3f1516
bohaohuang <bohao.huang@duke.edu>
2018-03-03 10:22:08 -0500

    deeplab spca random

--- ]tasks/2018.01.10.frrn/train_spca_deeplab_random.py
+++ ]tasks/2018.01.10.frrn/train_spca_deeplab_random.py
@@ -5,24 +5,27 @@ import tensorflow as tf
 import uabDataReader
 import uabRepoPaths
 import uabCrossValMaker
+import bohaoCustom.uabPreprocClasses as bPreproc
+import uabPreprocClasses
 import uab_collectionFunctions
 import uab_DataHandlerFunctions
-from bohaoCustom import uabMakeNetwork_UNet
+from bohaoCustom import uabMakeNetwork_DeepLabV2
 
 RUN_ID = 0
 BATCH_SIZE = 5
-LEARNING_RATE = 1e-4
-INPUT_SIZE = 572
-TILE_SIZE = 2048
-EPOCHS = 100
+LEARNING_RATE = 5e-8
+INPUT_SIZE = 321
+TILE_SIZE = 5000
+EPOCHS = 60
 NUM_CLASS = 2
 N_TRAIN = 8000
-N_VALID = 1000
+N_VALID = 2000
 GPU = 0
-DECAY_STEP = 60
+DECAY_STEP = 40
 DECAY_RATE = 0.1
 MODEL_NAME = 'spca_aug_random_{}'
 SFN = 32
+RES101_DIR = r'/hdd6/Models/resnet_v1_101.ckpt'
 
 
 def read_flag():
@@ -31,7 +34,7 @@ def read_flag():
     parser.add_argument('--learning-rate', type=float, default=LEARNING_RATE, help='learning rate (1e-3)')
     parser.add_argument('--input-size', default=INPUT_SIZE, type=int, help='input size 224')
     parser.add_argument('--tile-size', default=TILE_SIZE, type=int, help='tile size 5000')
-    parser.add_argument('--epochs', default=EPOCHS, type=int, help='# epochs (1)')
+    parser.add_argument('--epochs', default=EPOCHS, type= int, help='# epochs (1)')
     parser.add_argument('--num-classes', type=int, default=NUM_CLASS, help='# classes (including background)')
     parser.add_argument('--n-train', type=int, default=N_TRAIN, help='# samples per epoch')
     parser.add_argument('--n-valid', type=int, default=N_VALID, help='# patches to valid')
@@ -41,6 +44,7 @@ def read_flag():
     parser.add_argument('--model-name', type=str, default=MODEL_NAME, help='Model name')
     parser.add_argument('--run-id', type=str, default=RUN_ID, help='id of this run')
     parser.add_argument('--sfn', type=int, default=SFN, help='filter number of the first layer')
+    parser.add_argument('--res-dir', type=str, default=RES101_DIR, help='path to ckpt of Res101 model')
 
     flags = parser.parse_args()
     flags.input_size = (flags.input_size, flags.input_size)
@@ -55,16 +59,16 @@ def main(flags):
     X = tf.placeholder(tf.float32, shape=[None, flags.input_size[0], flags.input_size[1], 3], name='X')
     y = tf.placeholder(tf.int32, shape=[None, flags.input_size[0], flags.input_size[1], 1], name='y')
     mode = tf.placeholder(tf.bool, name='mode')
-    model = uabMakeNetwork_UNet.UnetModelCrop({'X':X, 'Y':y},
-                                              trainable=mode,
-                                              model_name=flags.model_name,
-                                              input_size=flags.input_size,
-                                              batch_size=flags.batch_size,
-                                              learn_rate=flags.learning_rate,
-                                              decay_step=flags.decay_step,
-                                              decay_rate=flags.decay_rate,
-                                              epochs=flags.epochs,
-                                              start_filter_num=flags.sfn)
+    model = uabMakeNetwork_DeepLabV2.DeeplabV3({'X':X, 'Y':y},
+                                               trainable=mode,
+                                               model_name=flags.model_name,
+                                               input_size=flags.input_size,
+                                               batch_size=flags.batch_size,
+                                               learn_rate=flags.learning_rate,
+                                               decay_step=flags.decay_step,
+                                               decay_rate=flags.decay_rate,
+                                               epochs=flags.epochs,
+                                               start_filter_num=flags.sfn)
     model.create_graph('X', class_num=flags.num_classes)
 
     # create collection
@@ -74,14 +78,15 @@ def main(flags):
     img_mean = blCol.getChannelMeans([1, 2, 3])         # get mean of rgb info
 
     # extract patches
-    extrObj = uab_DataHandlerFunctions.uabPatchExtrRand([0, 1, 2, 3], # extract all 4 channels
-                                                        cSize=flags.input_size, # patch size as 572*572
+    extrObj = uab_DataHandlerFunctions.uabPatchExtrRand([0, 1, 2, 3],  # extract all 4 channels
+                                                        cSize=flags.input_size,  # patch size as 572*572
                                                         numPerTile=256,  # overlap as 92
-                                                        extSave=['png', 'jpg', 'jpg', 'jpg'], # save rgb files as jpg and gt as png
+                                                        extSave=['png', 'jpg', 'jpg', 'jpg'],
+                                                        # save rgb files as jpg and gt as png
                                                         isTrain=True,
                                                         gtInd=0,
                                                         pad=model.get_overlap(),
-                                                        name='Rand{}'.format(flags.run_id)) # pad around the tiles
+                                                        name='Rand{}'.format(flags.run_id))  # pad around the tiles
     patchDir = extrObj.run(blCol)
 
     # make data reader
@@ -109,7 +114,7 @@ def main(flags):
                        loss_type='xent')
     model.run(train_reader=dataReader_train,
               valid_reader=dataReader_valid,
-              pretrained_model_dir=None,        # train from scratch, no need to load pre-trained model
+              pretrained_model_dir=flags.res_dir,
               isTrain=True,
               img_mean=img_mean,
               verb_step=100,                    # print a message every 100 step(sample)
