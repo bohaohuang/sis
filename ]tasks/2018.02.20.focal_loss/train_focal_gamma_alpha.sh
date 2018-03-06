e3d90fe639dab336183bed095bc260da4c4d3e8a
bohaohuang <bohao.huang@duke.edu>
2018-03-06 13:35:23 -0500

    focal loss scripts for DCC

---
+++ ]tasks/2018.02.20.focal_loss/train_focal_gamma_alpha.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -e slurm.err
+#SBATCH --mem=20G
+#SBATCH -c 6
+##SBATCH --exclude=dcc-gpu-[31-32]
+##SBATCH --exclude=dcc-collinslab-gpu-[02,04]
+#SBATCH -p collinslab --gres=gpu:1
+module load Anaconda3/3.5.2
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/opt/apps/rhel7/cudnn/lib64:$LD_LIBRARY_PATH
+export PYTHONPATH=$PYTHONPATH:/dscrhome/bh163/code/uab
+python train_spca_unet_focal.py --run-id=0 --gamma=0.5 --GPU=0
