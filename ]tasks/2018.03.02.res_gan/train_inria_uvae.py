e8a72ec189a6f3d9ad25d1c21ef3df3588ed045f
bohaohuang <bohao.huang@duke.edu>
2018-05-03 12:13:26 -0400

    gbdx unet

--- ]tasks/2018.03.02.res_gan/train_inria_uvae.py
+++ ]tasks/2018.03.02.res_gan/train_inria_uvae.py
@@ -9,23 +9,23 @@ import bohaoCustom.uabPreprocClasses as bPreproc
 import uabPreprocClasses
 import uab_collectionFunctions
 import uab_DataHandlerFunctions
-from bohaoCustom import uabMakeNetwork_UNet
+from bohaoCustom import uabMakeNetwork_UNetEncoder
 
 RUN_ID = 0
 BATCH_SIZE = 5
-LEARNING_RATE = 1e-3
-INPUT_SIZE = 572
-TILE_SIZE = 2541
+LEARNING_RATE = 1e-6
+INPUT_SIZE = 256
+TILE_SIZE = 5000
 EPOCHS = 100
 NUM_CLASS = 2
 N_TRAIN = 8000
-N_VALID = 2000
-GPU = 0
-DECAY_STEP = 40
+N_VALID = 1000
+GPU = 1
+DECAY_STEP = 60
 DECAY_RATE = 0.1
-MODEL_NAME = 'gbdx2_aug_grid_{}_sp'
+MODEL_NAME = 'inria_z{}_{}'
 SFN = 32
-RES101_DIR = r'/media/ei-edl01/data/uab_datasets/sp/]shared_models/UnetCropCV_(FixedRes)CTFinetune+nlayer9_PS(572, 572)_BS5_EP100_LR1e-05_DS50_DR0.1_SFN32'
+Z_DIM = 128
 
 
 def read_flag():
@@ -34,7 +34,7 @@ def read_flag():
     parser.add_argument('--learning-rate', type=float, default=LEARNING_RATE, help='learning rate (1e-3)')
     parser.add_argument('--input-size', default=INPUT_SIZE, type=int, help='input size 224')
     parser.add_argument('--tile-size', default=TILE_SIZE, type=int, help='tile size 5000')
-    parser.add_argument('--epochs', default=EPOCHS, type= int, help='# epochs (1)')
+    parser.add_argument('--epochs', default=EPOCHS, type=int, help='# epochs (1)')
     parser.add_argument('--num-classes', type=int, default=NUM_CLASS, help='# classes (including background)')
     parser.add_argument('--n-train', type=int, default=N_TRAIN, help='# samples per epoch')
     parser.add_argument('--n-valid', type=int, default=N_VALID, help='# patches to valid')
@@ -44,12 +44,12 @@ def read_flag():
     parser.add_argument('--model-name', type=str, default=MODEL_NAME, help='Model name')
     parser.add_argument('--run-id', type=str, default=RUN_ID, help='id of this run')
     parser.add_argument('--sfn', type=int, default=SFN, help='filter number of the first layer')
-    parser.add_argument('--res-dir', type=str, default=RES101_DIR, help='path to ckpt of Res101 model')
+    parser.add_argument('--z-dim', type=int, default=Z_DIM, help='dimension of latent variable')
 
     flags = parser.parse_args()
     flags.input_size = (flags.input_size, flags.input_size)
     flags.tile_size = (flags.tile_size, flags.tile_size)
-    flags.model_name = flags.model_name.format(flags.run_id)
+    flags.model_name = flags.model_name.format(flags.z_dim, flags.run_id)
     return flags
 
 
@@ -57,32 +57,32 @@ def main(flags):
     # make network
     # define place holder
     X = tf.placeholder(tf.float32, shape=[None, flags.input_size[0], flags.input_size[1], 3], name='X')
-    y = tf.placeholder(tf.int32, shape=[None, flags.input_size[0], flags.input_size[1], 1], name='y')
+    Y = tf.placeholder(tf.float32, shape=[None, flags.input_size[0], flags.input_size[1], 3], name='X')
     mode = tf.placeholder(tf.bool, name='mode')
-    model = uabMakeNetwork_UNet.UnetModelCrop({'X':X, 'Y':y},
-                                              trainable=mode,
-                                              model_name=flags.model_name,
-                                              input_size=flags.input_size,
-                                              batch_size=flags.batch_size,
-                                              learn_rate=flags.learning_rate,
-                                              decay_step=flags.decay_step,
-                                              decay_rate=flags.decay_rate,
-                                              epochs=flags.epochs,
-                                              start_filter_num=flags.sfn)
+    model = uabMakeNetwork_UNetEncoder.UnetVAE({'X':X, 'Y':Y},
+                                               trainable=mode,
+                                               model_name=flags.model_name,
+                                               input_size=flags.input_size,
+                                               batch_size=flags.batch_size,
+                                               learn_rate=flags.learning_rate,
+                                               decay_step=flags.decay_step,
+                                               decay_rate=flags.decay_rate,
+                                               epochs=flags.epochs,
+                                               start_filter_num=flags.sfn,
+                                               latent_num=flags.z_dim)
     model.create_graph('X', class_num=flags.num_classes)
 
     # create collection
     # the original file is in /ei-edl01/data/uab_datasets/inria
-    blCol = uab_collectionFunctions.uabCollection('gbdx2')
-    opDetObj = bPreproc.uabOperTileDivide(255)
-    rescObj = uabPreprocClasses.uabPreprocMultChanOp([], 'GT_Divide.tif', 'Map GT to (0, 1)', [0], opDetObj)
+    blCol = uab_collectionFunctions.uabCollection('inria')
+    opDetObj = bPreproc.uabOperTileDivide(255)          # inria GT has value 0 and 255, we map it back to 0 and 1
+    # [3] is the channel id of GT
+    rescObj = uabPreprocClasses.uabPreprocMultChanOp([], 'GT_Divide.tif', 'Map GT to (0, 1)', [3], opDetObj)
     rescObj.run(blCol)
-    blCol.readMetadata()
-    img_mean = blCol.getChannelMeans([1, 2, 3])         # get mean of rgb info
-    print(img_mean)
+    img_mean = blCol.getChannelMeans([0, 1, 2])         # get mean of rgb info
 
     # extract patches
-    extrObj = uab_DataHandlerFunctions.uabPatchExtr([1, 2, 3, 4], # extract all 4 channels
+    extrObj = uab_DataHandlerFunctions.uabPatchExtr([0, 1, 2, 4], # extract all 4 channels
                                                     cSize=flags.input_size, # patch size as 572*572
                                                     numPixOverlap=int(model.get_overlap()/2),  # overlap as 92
                                                     extSave=['jpg', 'jpg', 'jpg', 'png'], # save rgb files as jpg and gt as png
@@ -95,10 +95,8 @@ def main(flags):
     # use uabCrossValMaker to get fileLists for training and validation
     idx, file_list = uabCrossValMaker.uabUtilGetFolds(patchDir, 'fileList.txt', 'force_tile')
     # use first 5 tiles for validation
-    # file_list = [file_list[a] for a in np.random.permutation(len(file_list))] # permutate the files
-    valid_num = int(len(file_list) * 0.9)
-    file_list_train = file_list[:valid_num]
-    file_list_valid = file_list[valid_num:]
+    file_list_train = uabCrossValMaker.make_file_list_by_key(idx, file_list, [i for i in range(6, 37)])
+    file_list_valid = uabCrossValMaker.make_file_list_by_key(idx, file_list, [i for i in range(0, 6)])
 
     with tf.name_scope('image_loader'):
         # GT has no mean to subtract, append a 0 for block mean
@@ -114,15 +112,14 @@ def main(flags):
     # train
     start_time = time.time()
 
-    model.train_config('X', 'Y', flags.n_train, flags.n_valid, flags.input_size, uabRepoPaths.modelPath,
-                       loss_type='xent')
+    model.train_config('X', 'Y', flags.n_train, flags.n_valid, flags.input_size, uabRepoPaths.modelPath)
     model.run(train_reader=dataReader_train,
               valid_reader=dataReader_valid,
-              pretrained_model_dir=flags.res_dir,
+              pretrained_model_dir=None,
               isTrain=True,
               img_mean=img_mean,
               verb_step=100,                    # print a message every 100 step(sample)
-              save_epoch=2,                     # save the model every 5 epochs
+              save_epoch=5,                     # save the model every 5 epochs
               gpu=GPU,
               tile_size=flags.tile_size,
               patch_size=flags.input_size)
