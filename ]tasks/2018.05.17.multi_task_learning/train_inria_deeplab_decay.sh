01725eacde97fd33ef5c5ceb017a93b0936022fd
bohaohuang <bohao.huang@duke.edu>
2018-06-02 10:58:39 -0400

    change node

--- ]tasks/2018.05.17.multi_task_learning/train_inria_deeplab_decay.sh
+++ ]tasks/2018.05.17.multi_task_learning/train_inria_deeplab_decay.sh
@@ -4,7 +4,7 @@
 #SBATCH -c 6
 ##SBATCH --exclude=dcc-gpu-[31-32]
 ##SBATCH --exclude=dcc-collinslab-gpu-[02,03,04]
-#SBATCH -p gpu-common --gres=gpu:1
+#SBATCH -p collinslab --gres=gpu:1
 module load Anaconda3/3.5.2
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/opt/apps/rhel7/cudnn/lib64:$LD_LIBRARY_PATH
