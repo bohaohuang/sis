784dc8575ed4f2de7e5e80b1bc125f6c2b3fb5a8
bohaohuang <bohao.huang@duke.edu>
2018-10-08 15:09:53 -0400

    typo fix

--- ]tasks/2018.09.07.aemo/train_spca_pspnet.sh
+++ ]tasks/2018.09.07.aemo/train_spca_pspnet.sh
@@ -8,4 +8,4 @@
 module load Python-GPU/3.6.5
 export PYTHONPATH=$PYTHONPATH:/dscrhome/bh163/code/ersa
 export PYTHONPATH=$PYTHONPATH:/dscrhome/bh163/code/sis
-python train_spca_pspnet.py --decay-step=40 --decay-rate=0.1 --learning-rate=1e-4 --res-dir=/dscrhome/bh163/misc/pspnet101 --data-dir=/work/bh163/uab_datasets/spca
+python train_spca_pspnet.py --decay-step=40 --decay-rate=0.1 --learning-rate=1e-4 --res-dir=/dscrhome/bh163/misc/pspnet101 --data-dir=/work/bh163/uab_datasets/spca/data/Original_Tiles
