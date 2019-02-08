c9ea45e982d799c3083115d9ec5aa0238a40972d
bohaohuang <bohao.huang@duke.edu>
2019-02-08 14:13:28 -0500

    ssd config file

--- ]tasks/2018.11.16.transmission_line/train_ssd.sh
+++ ]tasks/2018.11.16.transmission_line/train_ssd.sh
@@ -1,8 +1,16 @@
-#!/usr/bin/env bash
-export CUDA_VISIBLE_DEVICES=1
+#!/bin/bash
+#SBATCH -e slurm.err
+#SBATCH --mem=20G
+#SBATCH -c 6
+##SBATCH --exclude=dcc-gpu-[31-32]
+##SBATCH --exclude=dcc-collinslab-gpu-[02,03,04]
+#SBATCH -p collinslab --gres=gpu:1
+export PYTHONPATH=$PYTHONPATH:/dscrhome/bh163/code/models/research:/dscrhome/bh163/code/models/research/slim
+module load Python-GPU/3.6.5
+
 TIME_STAMP=`date +%Y-%m-%d_%H-%M-%S`
-PIPELIN_CONFIG_PATH=/home/lab/Documents/bohao/data/transmission_line/ssd_resnet50.config
-MODEL_DIR=/home/lab/Documents/bohao/data/transmission_line/models/ssd_resnet50_${TIME_STAMP}
+PIPELIN_CONFIG_PATH=/dscrhome/bh163/code/sis/]tasks/2018.11.16.transmission_line/ssd_resnet50.config
+MODEL_DIR=/work/bh163/misc/object_detection/models/ssd_resnet50_${TIME_STAMP}
 python /home/lab/Documents/bohao/code/third_party/models/research/object_detection/model_main.py \
     --pipeline_config_path=${PIPELIN_CONFIG_PATH} \
     --model_dir=${MODEL_DIR} \
